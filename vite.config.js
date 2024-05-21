d13497cfae87bbf83a5cf5fe56503ddd58a9025d
AdeilsonDias <adeilsonj7dias@gmail.com>
2024-05-21 12:11:17 -0300

    new config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Buscar-CEP",
   plugins: [react()],
-  base:"/Buscador-de-CEP/",
+  
 })
